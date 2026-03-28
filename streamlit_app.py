1530d5e09198c1d5f70daab8375e37a44b323c04
Juvencia <132119547+Juvencia@users.noreply.github.com>
2026-03-28 12:05:39 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,12 @@
 import streamlit as st
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Spacer
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Spacer, Image
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib.units import cm
 from reportlab.lib import colors
 from io import BytesIO
+
+
+logo = Image("assets/rotaract_district_logo.png", width=2.5*cm, height=2.5*cm)
 
 st.set_page_config(page_title="Rotaract Invoice Generator")
 
@@ -100,7 +103,24 @@ if "invoice" in st.session_state:
         styles = getSampleStyleSheet()
 
         elements = []
-
+        header = Table([
+            [
+                logo,
+                Paragraph("<b>Rotaract District 3410<br/>Club Dues Invoice</b>", styles["Title"]),
+                Paragraph(f"<b>Invoice #</b><br/>{data['invoice_number']}", styles["Normal"])
+            ]
+        ], colWidths=[3*cm,11*cm,4*cm])
+        
+        header.setStyle(TableStyle([
+            ("BACKGROUND",(0,0),(-1,-1),colors.HexColor("#d81b60")),
+            ("TEXTCOLOR",(1,0),(2,0),colors.white),
+            ("VALIGN",(0,0),(-1,-1),"MIDDLE"),
+            ("LEFTPADDING",(0,0),(-1,-1),10)
+        ]))
+        
+        elements.append(header)
+        
+        elements.append
         elements.append(Paragraph("Rotaract Club - Club Dues Invoice", styles["Title"]))
 
         elements.append(Spacer(1,20))
